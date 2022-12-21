14056035903cddb124892c4830f9004961bc4019
Drew K Goodin <dkg@fastmail.com>
2022-12-20 19:32:21 -0800

    12-20-2022

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -11,10 +11,40 @@ var store = [{
         "url": "/breads%20and%20dough/2022/08/30/pizza-dough.html",
         "teaser": null
       },{
+        "title": "Bechamel",
+        "excerpt":" ","categories": ["Sauces"],
+        "tags": [],
+        "url": "/sauces/2022/09/12/bechamel.html",
+        "teaser": null
+      },{
+        "title": "Roasted Garlic Mac And Cheese",
+        "excerpt":" ","categories": ["Sides"],
+        "tags": [],
+        "url": "/sides/2022/10/15/roasted-garlic-mac-and-cheese.html",
+        "teaser": null
+      },{
+        "title": "White Beans With Ham Hock",
+        "excerpt":" ","categories": ["Sides"],
+        "tags": [],
+        "url": "/sides/2022/11/10/white-beans-with-ham-hock.html",
+        "teaser": null
+      },{
+        "title": "The Best Chicken Soup",
+        "excerpt":" ","categories": ["Soup"],
+        "tags": [],
+        "url": "/soup/2022/11/11/the-best-chicken-soup.html",
+        "teaser": null
+      },{
         "title": "Chilindron",
         "excerpt":" ","categories": ["Stews"],
         "tags": [],
         "url": "/stews/2022/11/29/chilindron.html",
+        "teaser": null
+      },{
+        "title": "Smoked And Seared Tri Tip",
+        "excerpt":" ","categories": ["Beef"],
+        "tags": [],
+        "url": "/beef/2022/12/01/smoked-and-seared-tri-tip.html",
         "teaser": null
       },{
         "title": "Chicken Thighs With Poppyseed Dumplings",
