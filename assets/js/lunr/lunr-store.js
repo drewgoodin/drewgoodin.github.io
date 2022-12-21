50ea5c63fb375d71f78f68c3d6ab180e89c48003
Drew K Goodin <dkg@fastmail.com>
2022-12-20 18:19:50 -0800

    12-20-2022

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,1 +1,25 @@
-var store = []
+var store = [{
+        "title": "Puff Pastry",
+        "excerpt":" ","categories": ["Breads and Dough"],
+        "tags": [],
+        "url": "/breads%20and%20dough/2022/08/14/puff-pastry.html",
+        "teaser": null
+      },{
+        "title": "Pizza Dough",
+        "excerpt":" ","categories": ["Breads and Dough"],
+        "tags": [],
+        "url": "/breads%20and%20dough/2022/08/30/pizza-dough.html",
+        "teaser": null
+      },{
+        "title": "Chilindron",
+        "excerpt":" ","categories": ["Stews"],
+        "tags": [],
+        "url": "/stews/2022/11/29/chilindron.html",
+        "teaser": null
+      },{
+        "title": "Chicken Thighs With Poppyseed Dumplings",
+        "excerpt":" ","categories": ["Chicken"],
+        "tags": [],
+        "url": "/chicken/2022/12/19/chicken-thighs-with-poppyseed-dumplings.html",
+        "teaser": null
+      }]
